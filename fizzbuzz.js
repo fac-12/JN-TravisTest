08e8afc51b6dcb8d7cf8c9f768f6fd4c91138621
Jamie Coe <jamiecoe66@gmail.com>
2017-10-31 13:57:14 +0000

    fixed coverage

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,16 +5,10 @@ function fizzbuzz(num) {
     return 'Fizz'
   } else if (num % 5 === 0) {
     return 'Buzz'
-  } else if (num > 10000) {
-    return 'never'
-  }
+  } 
   else {
     return num
   }
 }
 
 module.exports = fizzbuzz;
-
-function test() {
-
-}
